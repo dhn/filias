705c789c3bd54a50a28d818b9904a5cfa618b90e
Dennis Herrmann <dhn@4bit.ws>
2015-05-05 19:05:48 +0200

    - Make pep8 happy

--- sql.py
+++ sql.py
@@ -4,7 +4,8 @@
 import sqlite3
 
 # conn = sqlite3.connect(":memory:")
-conn = sqlite3.connect("./test.sql")
+conn = sqlite3.connect("./ILIAS.sql")
+
 
 # create SQL table
 def create_db():
@@ -22,8 +23,8 @@ def create_db():
                 UNIQUE (title, obj_id)) \
             ")
             state = True
-    except sqlite3.Error as e:
-        print("An error occurred:", e.args[0])
+    except sqlite3.Error as error:
+        print("An error occurred:", error.args[0])
         conn.close()
     return state
 
@@ -33,10 +34,12 @@ def insert_into_db(title, obj_id, parent_id, last_update, file_size, types):
     try:
         with conn:
             conn.execute(" \
-                INSERT OR IGNORE INTO files (title, obj_id, parent_id, last_update, file_size, types) \
-                VALUES (?, ?, ?, ?, ?, ?)", (title, obj_id, parent_id, last_update, file_size, types))
-    except sqlite3.Error as e:
-        print("An error occurred:", e.args[0])
+                INSERT OR IGNORE INTO files (title, obj_id, parent_id, \
+                                             last_update, file_size, types) \
+                VALUES (?, ?, ?, ?, ?, ?)", (title, obj_id, parent_id,
+                                             last_update, file_size, types))
+    except sqlite3.Error as error:
+        print("An error occurred:", error.args[0])
         conn.close()
 
 
@@ -47,8 +50,8 @@ def getCourseName():
             result = conn.execute("SELECT title FROM files \
                     WHERE types='crs' \
                     ORDER BY title").fetchall()
-    except sqlite3.Error as e:
-        print("An error occurred:", e.args[0])
+    except sqlite3.Error as error:
+        print("An error occurred:", error.args[0])
         conn.close()
     return result
 
@@ -58,9 +61,10 @@ def getParentTitle(obj_id):
     try:
         with conn:
             result = conn.execute("SELECT title FROM files \
-                    WHERE obj_id = (SELECT parent_id FROM files where obj_id = ?)", (obj_id,)).fetchone()
-    except sqlite3.Error as e:
-        print("An error occurred:", e.args[0])
+                    WHERE obj_id = (SELECT parent_id FROM files \
+                    WHERE obj_id = ?)", (obj_id,)).fetchone()
+    except sqlite3.Error as error:
+        print("An error occurred:", error.args[0])
         conn.close()
     return result
 
@@ -70,7 +74,7 @@ def getAllInformation():
     try:
         with conn:
             result = conn.execute("SELECT * FROM files").fetchall()
-    except sqlite3.Error as e:
-        print("An error occurred:", e.args[0])
+    except sqlite3.Error as error:
+        print("An error occurred:", error.args[0])
         conn.close()
     return result
